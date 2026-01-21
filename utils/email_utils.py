2ecd0d81b496a530e69a389ce80ec101b796a4bc
amal2312 <amal.shaji023@gmail.com>
2026-01-21 14:37:22 +0530

    Gracefully handle Brevo SDK absence in cloud environment

--- utils/email_utils.py
+++ utils/email_utils.py
@@ -1,39 +1,34 @@
-import streamlit as st
-import sib_api_v3_sdk
-from sib_api_v3_sdk.rest import ApiException
+try:
+    import sib_api_v3_sdk
+    from sib_api_v3_sdk.rest import ApiException
+    BREVO_AVAILABLE = True
+except ImportError:
+    BREVO_AVAILABLE = False
 
 
 def send_confirmation_email(to_email, booking_id, booking_data):
+    if not BREVO_AVAILABLE:
+        raise Exception("Brevo email service not available in this environment")
+
     configuration = sib_api_v3_sdk.Configuration()
     configuration.api_key["api-key"] = st.secrets["BREVO_API_KEY"]
 
-    api_client = sib_api_v3_sdk.ApiClient(configuration)
-    api_instance = sib_api_v3_sdk.TransactionalEmailsApi(api_client)
-
-    subject = "Doctor Appointment Confirmation"
-    body = f"""
-Hello {booking_data['name']},
-
-Your doctor appointment has been confirmed.
-
-Booking ID: {booking_id}
-Date: {booking_data['date']}
-Time: {booking_data['time']}
-
-Thank you.
-"""
-
-    email = sib_api_v3_sdk.SendSmtpEmail(
-        sender={
-            "email": st.secrets["BREVO_SENDER_EMAIL"],
-            "name": st.secrets["BREVO_SENDER_NAME"]
-        },
-        to=[{"email": to_email}],
-        subject=subject,
-        text_content=body
+    api_instance = sib_api_v3_sdk.TransactionalEmailsApi(
+        sib_api_v3_sdk.ApiClient(configuration)
     )
 
-    try:
-        api_instance.send_transac_email(email)
-    except ApiException as e:
-        raise Exception(str(e))
+    email = sib_api_v3_sdk.SendSmtpEmail(
+        to=[{"email": to_email}],
+        sender={"email": st.secrets["BREVO_SENDER_EMAIL"]},
+        subject="Doctor Appointment Confirmation",
+        html_content=f"""
+        <h2>Appointment Confirmed</h2>
+        <p><b>Booking ID:</b> {booking_id}</p>
+        <p><b>Name:</b> {booking_data['name']}</p>
+        <p><b>Date:</b> {booking_data['date']}</p>
+        <p><b>Time:</b> {booking_data['time']}</p>
+        """
+    )
+
+    api_instance.send_transac_email(email)
+
